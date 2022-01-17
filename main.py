1e9af13ae4c96c3b4256e0744dc8c28bb608cd91
Steven Beeching <steven.beeching@gmail.com>
2022-01-17 21:57:43 +0000

    Add starting message

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import os
 s = sched.scheduler(time.time, time.sleep)
 rt = os.environ['REFRESH_TIME']
 
+print ("Bulb data gathering started, refreshing every {} seconds".format(rt))
+
 def loop(sc):
     bulb.retrieveBulbData((datetime.today() - timedelta(days=6)).strftime('%Y-%m-%dT00:00:00.000Z'), (datetime.today() + timedelta(days=1)).strftime('%Y-%m-%dT00:00:00.000Z'))
     
