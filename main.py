3ba9eab90d11eda18ce188e62451ea0632c8a568
Steven Beeching <steven.beeching@gmail.com>
2022-01-17 21:39:13 +0000

    Look forward

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 s = sched.scheduler(time.time, time.sleep)
 
 def loop(sc):
-    bulb.retrieveBulbData((datetime.today() - timedelta(days=6)).strftime('%Y-%m-%dT00:00:00.000Z'), datetime.today().strftime('%Y-%m-%dT00:00:00.000Z'))
+    bulb.retrieveBulbData((datetime.today() - timedelta(days=6)).strftime('%Y-%m-%dT00:00:00.000Z'), (datetime.today() + timedelta(days=1)).strftime('%Y-%m-%dT00:00:00.000Z'))
     
     s.enter(900, 1, loop, (sc,))
 
